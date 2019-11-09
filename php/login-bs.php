89715149933967ec3e36ae3a0cae9e0484c058c7
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-11-09 11:21:13 +0100

    Add warnings for incorrect register data

--- php/login-bs.php
+++ php/login-bs.php
@@ -1,12 +1,11 @@
 <?php
-include "/lib/mainLib.php";
+include "./lib/mainLib.php";
 startSessionIfNot();
 
 if (isLoggedIn()) {
     header('Location: addIncome-bs.php');
     exit();
 }
-
 ?>
 
 
@@ -30,10 +29,11 @@ if (isLoggedIn()) {
     <link rel="stylesheet" href="../css/main.css">
 </head>
 
+
 <body>
     <header>
         <nav class="navbar navbar-dark bg-custom navbar-expand-lg fixed-top">
-            <a class="navbar-brand" href="login-bs.html"><img src="img/logo.png" width="30" height="30" class="d-inline-block mr-1 align-bottom" alt="">
+            <a class="navbar-brand" href="../index.php"><img src="../img/logo.png" width="36" height="36" class="d-inline-block mr-1 align-bottom" alt="">
                 <span class="h3 text-warning">Budżet osobisty </span>
             </a>
             <?php
@@ -43,6 +43,7 @@ if (isLoggedIn()) {
                 echo '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainmenu" aria-controls="mainmenu" aria-expanded="false" aria-label="Przełącznik nawigacji">';
             }
             ?>
+
             <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -61,7 +62,7 @@ if (isLoggedIn()) {
                     </li>
                 </ul>
 
-                <form class="form-inline" action="../php/login.php" method="POST">
+                <form class="form-inline" action="login.php" method="POST">
                     <?php
                     if (isset($_SESSION['incorrectLogin'])) {
                         echo '<input style="border-width:3px;" class="form-control border-danger mr-1 my-1" name="login" type="text" placeholder="błędny login" required>';
@@ -88,72 +89,89 @@ if (isLoggedIn()) {
         <div class="container move-under-navbar">
             <div class="row">
                 <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3">
-                    <form action="../php/" method="POST">
-                        <div class="card shadow-lg mb-5 bg-white rounded">
-                            <div class="card-header bg-info card-topic text-center"> Rejestracja </div>
-                            <div class="card-body">
-                                <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="material-icons">account_box</i>
-                                        </span>
+
+                        <form action="register.php" method="POST">
+                            <div class="card shadow-lg mb-5 bg-white rounded">
+                                <div class="card-header bg-info card-topic text-center"> Rejestracja </div>
+                                <div class="card-body">
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="material-icons">account_box</i>
+                                            </span>
+                                        </div>
+                                        <input type="text" class="form-control" name="nickName" placeholder="Nazwa użytkownika" minlength="6" maxlength="50" required>
                                     </div>
-                                    <input type="text" class="form-control" name="firstName" placeholder="Imię" maxlength="64" required>
-                                </div>
+                                    <?php
+                                        if (isset($_SESSION['e_nick'])) {
+                                            echo '<p class="text-danger text-center">' . $_SESSION['e_nick'] . '</p>';
+                                            unset($_SESSION['e_nick']);
+                                        }
+                                        ?>
 
-                                <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="material-icons">email</i>
-                                        </span>
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="material-icons">email</i>
+                                            </span>
+                                        </div>
+                                        <input type="email" class="form-control" name="email" placeholder="Adres email" minlength="6" maxlength="50" required>
                                     </div>
-                                    <input type="email" class="form-control" name="email" placeholder="Adres email" maxlength="64" required>
-                                </div>
+                                    <?php
+                                        if (isset($_SESSION['e_email'])) {
+                                            echo '<p class="text-danger text-center">' . $_SESSION['e_email'] . '</p>';
+                                            unset($_SESSION['e_email']);
+                                        }
+                                        ?>
 
-                                <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="material-icons">lock</i>
-                                        </span>
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="material-icons">lock</i>
+                                            </span>
+                                        </div>
+                                        <input type="password" class="form-control" name="password" placeholder="Hasło" minlength="8" maxlength="64" required>
                                     </div>
-                                    <input type="password" class="form-control" name="password" placeholder="Hasło" minlength="8" maxlength="64" required>
-                                </div>
+                                    <?php
+                                        if (isset($_SESSION['e_pwd'])) {
+                                            echo '<p class="text-danger text-center">' . $_SESSION['e_pwd'] . '</p>';
+                                            unset($_SESSION['e_pwd']);
+                                        }
+                                        ?>
 
-                                <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="material-icons">lock</i>
-                                        </span>
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="material-icons">lock</i>
+                                            </span>
+                                        </div>
+                                        <input type="password" class="form-control" name="confirmPassword" placeholder="Potwierdź hasło" minlength="8" maxlength="64" required>
                                     </div>
-                                    <input type="password" class="form-control" name="confirmPassword" placeholder="Potwierdź hasło" minlength="8" maxlength="64" required>
+                                    <?php
+                                        if (isset($_SESSION['e_confirmPwd'])) {
+                                            echo '<p class="text-danger text-center">' . $_SESSION['e_confirmPwd'] . '</p>';
+                                            unset($_SESSION['e_confirmPwd']);
+                                        }
+                                        ?>
                                 </div>
 
-                                <div class="form-group form-check">
-                                    <label class="form-check-label">
-                                        <input class="form-check-input" type="checkbox">
-                                        Zapamiętaj mnie
-                                    </label>
-                                </div>
-                            </div>
-
-                            <div class="card-footer bg-info text-white ">
-                                <div class="row">
-                                    <div class="col-md-12 text-center">
-                                        <div class="btn-group">
-                                            <button class="btn btn-success btn-sm px-2" type="submit">
-                                                <i class="material-icons">person_add</i>
-                                            </button>
-                                            <button class="btn btn-warning btn-sm pl-0 text-button" type="submit">
-                                                Zarejestruj się
-                                            </button>
+                                <div class="card-footer bg-info text-white ">
+                                    <div class="row">
+                                        <div class="col-md-12 text-center">
+                                            <div class="btn-group">
+                                                <button class="btn btn-success btn-sm px-2" type="submit">
+                                                    <i class="material-icons">person_add</i>
+                                                </button>
+                                                <button class="btn btn-warning btn-sm pl-0 text-button" type="submit">
+                                                    Zarejestruj się
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-
                             </div>
-                        </div>
-                    </form>
-                </div>
+                        </form>
+              </div>
             </div>
         </div>
     </main>
