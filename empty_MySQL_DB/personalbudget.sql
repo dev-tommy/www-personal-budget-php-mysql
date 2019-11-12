32f3eec2a3a8cf11b8aa5c7a4f317d0d22432f88
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2019-11-12 20:40:40 +0100

    Add default website  mysql database

---
+++ empty_MySQL_DB/personalbudget.sql
@@ -0,0 +1,226 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 12 Lis 2019, 20:32
+-- Wersja serwera: 10.4.6-MariaDB
+-- Wersja PHP: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `personalbudget`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `expenses`
+--
+
+CREATE TABLE `expenses` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `expense_category_assigned_to_user_id` int(11) UNSIGNED NOT NULL,
+  `payment_method_assigned_to_user_id` int(11) UNSIGNED NOT NULL,
+  `amount` decimal(8,2) NOT NULL DEFAULT 0.00,
+  `date_of_expense` date NOT NULL,
+  `expense_comment` varchar(180) COLLATE utf8_polish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `expenses_category_default`
+--
+
+CREATE TABLE `expenses_category_default` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `expenses_category_default`
+--
+
+INSERT INTO `expenses_category_default` (`id`, `name`) VALUES
+(1, 'Jedzenie'),
+(2, 'Mieszkanie'),
+(3, 'Transport'),
+(4, 'Telekomunikacja'),
+(5, 'Opieka zdrowotna'),
+(6, 'Ubranie'),
+(7, 'Higiena'),
+(8, 'Dzieci'),
+(9, 'Rozrywka'),
+(10, 'Wycieczka'),
+(11, 'Szkolenie'),
+(12, 'Książki'),
+(13, 'Oszczędności'),
+(14, 'Na emeryturę'),
+(15, 'Spłata długów'),
+(16, 'Darowizna');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `incomes`
+--
+
+CREATE TABLE `incomes` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `income_category_assigned_to_user_id` int(11) UNSIGNED NOT NULL,
+  `amount` decimal(8,2) NOT NULL DEFAULT 0.00,
+  `date_of_income` date NOT NULL,
+  `income_comment` varchar(180) COLLATE utf8_polish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `incomes_category_default`
+--
+
+CREATE TABLE `incomes_category_default` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `incomes_category_default`
+--
+
+INSERT INTO `incomes_category_default` (`id`, `name`) VALUES
+(1, 'Wynagrodzenie'),
+(2, 'Odsetki bankowe'),
+(3, 'Sprzedaż Allegro'),
+(4, 'Inne');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `payment_methods_default`
+--
+
+CREATE TABLE `payment_methods_default` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `payment_methods_default`
+--
+
+INSERT INTO `payment_methods_default` (`id`, `name`) VALUES
+(1, 'Gotówka'),
+(2, 'Karta debetowa'),
+(3, 'Karta kredytowa');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `username` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_polish_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `expenses`
+--
+ALTER TABLE `expenses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `expenses_category_default`
+--
+ALTER TABLE `expenses_category_default`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `incomes`
+--
+ALTER TABLE `incomes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `incomes_category_default`
+--
+ALTER TABLE `incomes_category_default`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `payment_methods_default`
+--
+ALTER TABLE `payment_methods_default`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `expenses`
+--
+ALTER TABLE `expenses`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT dla tabeli `expenses_category_default`
+--
+ALTER TABLE `expenses_category_default`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT dla tabeli `incomes`
+--
+ALTER TABLE `incomes`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT dla tabeli `incomes_category_default`
+--
+ALTER TABLE `incomes_category_default`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT dla tabeli `payment_methods_default`
+--
+ALTER TABLE `payment_methods_default`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
